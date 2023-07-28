58e8125738ae6b01f33caf0b3d32441f12b60cff
Vitaliy <vetal.com92@gmail.com>
2023-07-28 21:08:27 +0600

    TestNG1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,9 +9,9 @@ public class CashbackHackServiceTest {
     public void testInsert() {
         CashbackHackService service = new CashbackHackService();
         int expected1 = 990;
-        Assert.assertEquals(expected1, service.remain(8));
+        Assert.assertEquals(expected1, service.remain(10));
         int expected2 = 1010;
-        Assert.assertEquals(expected2, service.remain(-8));
+        Assert.assertEquals(expected2, service.remain(-10));
         int expected3 = 1000;
         Assert.assertEquals(expected3, service.remain(0));
 
